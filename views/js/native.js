41a9e29faefb97d26f585088b5c224c2dead9fb5
samhigg <samhigg@iu.edu>
2025-05-05 20:42:18 -0400

    added quiz with functionality through firebase

--- views/js/native.js
+++ views/js/native.js
@@ -1,3 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { doc, addDoc, getDocs, collection, getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyC97sfVBB0UE3LtsDNYYVY8qxMhSwO_vus",
+    authDomain: "n320-baklava.firebaseapp.com",
+    projectId: "n320-baklava",
+    storageBucket: "n320-baklava.firebasestorage.app",
+    messagingSenderId: "208778952203",
+    appId: "1:208778952203:web:6953ec18e371a85d96510e"
+  };
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const myDoc = doc(db, "nativeQuiz", "2n7n0kxt3hq4yvc0UK9T");
+
 const storyTitle = document.getElementById("storyTitle");
 const storyText = document.getElementById("storyText");
 
@@ -5,6 +19,13 @@ const owlBtn = document.getElementById("owlBtn");
 const woodpeckerBtn = document.getElementById("woodpeckerBtn");
 const coyoteBtn = document.getElementById("coyoteBtn");
 const moleBtn = document.getElementById("moleBtn");
+
+const submitBtn = document.getElementById("submit");
+
+submitBtn.onclick = function(e){
+    e.preventDefault();
+    quiz();
+};
 
 owlBtn.onclick = function(){changeText("OWL DESCRIBES HIMSELF", `"I am called Yî, 'owl'. I carry a basket and frequent the low gaps looking for people. I sing for them but do not think about them." "If you come to see me, you may eat the leg which lies in the basket," he told him. "There is nothing in it," his guest replied. "Yes, there is pemmican in it," he said. "That is my luncheon. When the sun goes down my basket will be full of meat." "Where shall I come to see you?" he asked. "You may come to me where the two cedars stand." He came there at evening and found his friend with a basket full of meat. The pemmican that had been in it was gone. "This is something's meat; take it home to your children." Then he carried it to his children. "I am carrying it home to my children," he said. "There are not many of them, there are only two." "Any way I am pleased, for I was looking for provisions. My children eat nothing but meat. They become large quickly because there are only two of them." "The people fear me because of my eyes. They are afraid also of my yellow horns. This is my nature. I bring you people's meat and I say to him, 'whu o whu!'" Thus he spoke they say.`)};
 
@@ -17,4 +38,120 @@ moleBtn.onclick = function(){changeText("HOW MOLE WON THE RACE", `All the men co
 function changeText(title, text) {
     storyTitle.innerHTML = (`<h2>${title}</h2>`)
     storyText.innerHTML = (`<p>${text}</p>`)
+}
+
+async function quiz() {
+    var owlScore = 0;
+    var woodpeckerScore = 0;
+    var coyoteScore = 0;
+    var moleScore = 0;
+
+    if (document.getElementById("answer11").checked) {
+        moleScore += 0.75;
+        coyoteScore += 1;
+        owlScore += 0.25;
+    }
+
+    if (document.getElementById("answer12").checked) {
+        woodpeckerScore += 1;
+    }
+
+    if (document.getElementById("answer13").checked) {
+        owlScore += 0.75;
+        moleScore + 0.25;
+    }
+
+    if (document.getElementById("answer21").checked) {
+        woodpeckerScore += 0.75;
+        owlScore += 0.5;
+    }
+
+    if (document.getElementById("answer22").checked) {
+        moleScore += 0.5;
+        owlScore += 0.5;
+        woodpeckerScore += 0.25;
+    }
+
+    if (document.getElementById("answer23").checked) {
+        moleScore += 0.5;
+        coyoteScore += 1;
+    }
+
+    if (document.getElementById("answer31").checked) {
+        moleScore += 0.75;
+        coyoteScore += 0.75;
+    }
+
+    if (document.getElementById("answer32").checked) {
+        woodpeckerScore += 0.75;
+        moleScore += 0.25;
+        owlScore += 0.25;
+    }
+
+    if (document.getElementById("answer33").checked) {
+        owlScore += 0.75;
+        coyoteScore += 0.25;
+    }
+
+    if(owlScore > woodpeckerScore && owlScore > coyoteScore && owlScore > moleScore) {
+        document.getElementById("result").innerHTML = "<h2>You are most like the Owl!</h2>"
+    } else if(woodpeckerScore > owlScore && woodpeckerScore > coyoteScore && woodpeckerScore > moleScore) {
+        document.getElementById("result").innerHTML = "<h2>You are most like the Woodpecker!</h2>"
+    } else if(coyoteScore > woodpeckerScore && coyoteScore > owlScore && coyoteScore > moleScore) {
+        document.getElementById("result").innerHTML = "<h2>You are most like the Coyote!</h2>"
+    } else if(moleScore > woodpeckerScore && moleScore > coyoteScore && moleScore > owlScore) {
+        document.getElementById("result").innerHTML = "<h2>You are most like the Mole!</h2>"
+    } else {
+        document.getElementById("result").innerHTML = "<h2>You are a mixed bag! Try changing an answer or two and check again!</h2>"
+    }
+
+    owlScore = owlScore * 30;
+    woodpeckerScore = woodpeckerScore * 30;
+    coyoteScore = coyoteScore * 30;
+    moleScore = moleScore * 30;
+
+    document.getElementById("owlResult").innerHTML = ("Owl: " + owlScore + "%");
+    document.getElementById("woodpeckerResult").innerHTML = ("Woodpecker: " + woodpeckerScore + "%");
+    document.getElementById("coyoteResult").innerHTML = ("Coyote: " + coyoteScore + "%");
+    document.getElementById("moleResult").innerHTML = ("Mole: " + moleScore + "%");
+
+    if (confirm("Would you like to save your results?")) {
+        var myName = prompt("Please enter your name.")
+        try {
+            const docRef = await addDoc(collection(db, "nativeQuiz"), {
+                name: myName,
+                owlScore: owlScore,
+                woodpeckerScore: woodpeckerScore,
+                coyoteScore: coyoteScore,
+                moleScore: moleScore
+            });
+            console.log("Document written with ID: ", docRef.id);
+
+            const nativeQuizCollection = collection(db, "nativeQuiz"); 
+
+            try {
+            const querySnapshot = await getDocs(nativeQuizCollection);
+            const quizzes = [];
+            querySnapshot.forEach((doc) => {
+                var thisJSON = (JSON.stringify(doc.data()));
+                var thisElement = (JSON.parse(thisJSON));
+
+                console.log(thisJSON);
+
+                var thisClasses = "jsonResult";
+
+                if (myName === thisElement["name"] && owlScore === thisElement["owlScore"] && woodpeckerScore === thisElement["woodpeckerScore"] && coyoteScore === thisElement["coyoteScore"] && moleScore === thisElement["moleScore"]) {
+                    thisClasses += " myResult"
+                }
+
+                document.getElementById("results").innerHTML +=`<div class="${thisClasses}"><b>${thisElement["name"]}</b><p>Owl: ${thisElement["owlScore"]}%</p><p>Woodpecker: ${thisElement["woodpeckerScore"]}%</p><p>Coyote: ${thisElement["coyoteScore"]}%</p><p>Mole: ${thisElement["moleScore"]}%</p></div>`;
+            });
+
+            } catch (e) {
+            console.error("Error getting documents: ", e);
+            }
+        } catch (error) {
+            console.error("Error adding document: ", error);
+        }
+    }
 }
