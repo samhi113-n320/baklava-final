8820dd6e1b2f86d2a55cbc594ee7e879e13ea5f3
auscohen-IU <auscohen@iu.edu>
2025-05-06 19:14:08 -0400

    Finished up the greek.html and greek.js. Added native section to the index.html. Updated styles.css to better match group styling in the html.

--- views/js/greek.js
+++ views/js/greek.js
@@ -3,25 +3,52 @@ console.log("Greek.js loaded successfully");
 
 // Data for characters
 const characterDetails = {
-    zeus: "Zeus is the king of the gods in Greek mythology. He rules Mount Olympus and is associated with thunder, lightning, and the sky.",
-    hera: "Hera is the queen of the gods and the goddess of marriage and family. She is known for her jealousy and her role as Zeus's wife.",
-    athena: "Athena is the goddess of wisdom, courage, and warfare. She is often depicted with an owl and a shield.",
-    poseidon: "Poseidon is the god of the sea, earthquakes, and horses. He is known for his trident and his temper."
+    zeus: {
+        info: "Zeus is the king of the gods in Greek mythology. He rules Mount Olympus and is associated with thunder, lightning, and the sky.",
+        link: "https://en.wikipedia.org/wiki/Zeus"
+    },
+    hera: {
+        info: "Hera is the queen of the gods and the goddess of marriage and family. She is known for her jealousy and her role as Zeus's wife.",
+        link: "https://en.wikipedia.org/wiki/Hera"
+    },
+    athena: {
+        info: "Athena is the goddess of wisdom, courage, and warfare. She is often depicted with an owl and a shield.",
+        link: "https://en.wikipedia.org/wiki/Athena"
+    },
+    poseidon: {
+        info: "Poseidon is the god of the sea, earthquakes, and horses. He is known for his trident and his temper.",
+        link: "https://en.wikipedia.org/wiki/Poseidon"
+    }
 };
 
 // Data for myths
 const mythDetails = {
-    heracles: "The Twelve Labors of Heracles are a series of tasks performed by Heracles as penance for killing his family.",
-    odyssey: "The Odyssey is an epic poem by Homer that tells the story of Odysseus's journey home after the Trojan War.",
-    iliad: "The Iliad is another epic poem by Homer that focuses on the events of the Trojan War.",
-    persephone: "The Myth of Persephone explains the changing seasons. Persephone is taken to the underworld by Hades, causing her mother Demeter to grieve."
+    heracles: {
+        info: "The Twelve Labors of Heracles are a series of tasks performed by Heracles as penance for killing his family.",
+        link: "https://en.wikipedia.org/wiki/Labours_of_Heracles"
+    },
+    odyssey: {
+        info: "The Odyssey is an epic poem by Homer that tells the story of Odysseus's journey home after the Trojan War.",
+        link: "https://en.wikipedia.org/wiki/Odyssey"
+    },
+    iliad: {
+        info: "The Iliad is another epic poem by Homer that focuses on the events of the Trojan War.",
+        link: "https://en.wikipedia.org/wiki/Iliad"
+    },
+    persephone: {
+        info: "The Myth of Persephone explains the changing seasons. Persephone is taken to the underworld by Hades, causing her mother Demeter to grieve.",
+        link: "https://en.wikipedia.org/wiki/Persephone"
+    }
 };
 
 // Function to show character info
 window.showCharacterInfo = (id) => {
     const characterInfo = document.getElementById("character-info");
     if (characterDetails[id]) {
-        characterInfo.innerHTML = `<p>${characterDetails[id]}</p>`;
+        characterInfo.innerHTML = `
+            <p>${characterDetails[id].info}</p>
+            <p><a href="${characterDetails[id].link}" target="_blank">Would you like to learn more?</a></p>
+        `;
     }
 };
 
@@ -29,6 +56,9 @@ window.showCharacterInfo = (id) => {
 window.showMythInfo = (id) => {
     const mythInfo = document.getElementById("myth-info");
     if (mythDetails[id]) {
-        mythInfo.innerHTML = `<p>${mythDetails[id]}</p>`;
+        mythInfo.innerHTML = `
+            <p>${mythDetails[id].info}</p>
+            <p><a href="${mythDetails[id].link}" target="_blank">Would you like to learn more?</a></p>
+        `;
     }
 };
